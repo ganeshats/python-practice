4fd5409c8337e310b162c003702f618d9d9f7cea
Ganesha TS <ganesha@c-lglgomathi.realimage.co.in>
2017-11-23 19:17:09 +0530

    Implementing review comments

--- OddEvenSort.py
+++ OddEvenSort.py
@@ -1,4 +1,4 @@
-in_numbers = raw_input("Enter some numbers with comma separated: ")
+in_numbers = raw_input("Enter some numbers with comma separated: ").strip()
 even_list = []
 odd_list = []
 for x in in_numbers.split(","):
